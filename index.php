632a32b6d682623248b78aee906a5ea0a1e0dfb1
evaod <114713729@umail.ucc.ie>
2018-02-23 16:56:43 +0000

    new

--- index.php
+++ index.php
@@ -36,7 +36,12 @@
  
  
   <body id="page-top">
-
+    <!--jquery-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <!--complete bootstrap javascript-->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    
+    
     <!-- Navigation -->
     <!-- Learned about chaanging colour of text and background in navbar here
           https://stackoverflow.com/questions/18529274/change-navbar-color-in-twitter-bootstrap-3-->
